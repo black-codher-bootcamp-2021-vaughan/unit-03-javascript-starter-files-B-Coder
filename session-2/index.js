58b0094efdb517fcc1c092e3579312b04297829e
B-Coder <97311741+B-Coder@users.noreply.github.com>
2022-06-21 20:19:41 +0100

    change

--- session-2/index.js
+++ session-2/index.js
@@ -1,3 +1,6 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
 console.log('hi everyone')
+console.log(document.getElementById('description'))
+VM864:1 <p id=​"description" black codher>​</p>​
+console.log
